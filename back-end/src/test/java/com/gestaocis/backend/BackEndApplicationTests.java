6864cdb343ff19b61a6dc5377ccf85f7204c5db1
Fabio Frasson <fabio.frass@gmail.com>
2021-08-10 19:57:26 -0300

    Back-end project upload

---
+++ back-end/src/test/java/com/gestaocis/backend/BackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestaocis.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

06141e029e06c73c6f719d80e87d996d3de7a124
alinezsigmond <alinezsigmond@hotmail.com>
2021-08-30 13:20:35 -0300

    Sidebar

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 import Header from './Components/Header/Header';
+import Sidebar from './Components/Sidebar/Sidebar';
 
 function App() {
   return (
+    <>
     <Header name='Meu Anjo' />
+    <Sidebar />
+    </>
   );
 }
 

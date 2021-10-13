6a371adb2b1d3e83e810e3c0f16ec5da61d24032
Mariana10-04 <71359480+Mariana10-04@users.noreply.github.com>
2021-10-13 17:20:39 -0300

    controller-services.room

--- back-end/src/main/java/com/gestaocis/backend/controllers/RoomController.java
+++ back-end/src/main/java/com/gestaocis/backend/controllers/RoomController.java
@@ -1,81 +1,50 @@
 package com.gestaocis.backend.controllers;
 
-import com.gestaocis.backend.models.User;
+import com.gestaocis.backend.exceptions.BadRequestException;
+import com.gestaocis.backend.services.RoomService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import java.util.UUID;
 
+@RestController
 @RequestMapping("/api/salas")
 public class RoomController {
-    private final RoomService service;
+
+    @Autowired
+    private RoomController
 
     @Autowired
     public RoomController(RoomService service) {
         this.service = service;
     }
 
-    @PostMapping
-    public ResponseEntity<?> createRoom(@RequestBody User room) throws Exception {
+    @PostMapping //nova sala
+    public ResponseEntity<RoomResponseDTO> save(@RequestBody NewRoomRequestDTO responseBody) throws Exception {
+        return new ResponseEntity<>(this.service.save(responseBody), HttpStatus.CREATED);
+    }
+
+    @GetMapping(path ="/{uuid}") // UUID= "unicos"
+    public ResponseEntity<RoomResponseDTO> findRoomByUUID(@RequestParam UUID uuid){
+        return new ResponseEntity<>(this.service.findByUUID(uuid), HttpStatus.OK);
+    }
+
+    @PutMapping(path= "/{uuid}")
+    public final ResponseEntity<RoomResponseDTO> updatePatient(@RequestParam UUID uuid, @RequestBody NewRoomRequestDTO responseBody){
         try{
-            User saved = service.create(room);
-            return new ResponseEntity<>(saved, null, HttpStatus.CREATED);
-        }
-        catch(Exception e){
-            throw new Exception(e);
+            return new ResponseEntity<>(this.service.update(uuid, responseBody), HttpStatus.OK);
+        }catch (Exception exception){
+            throw new BadRequestException(exception.getMessage());
         }
     }
 
-    @GetMapping
-    public ResponseEntity<?> findAll() throws Exception{
-        try{
-            List<User> found = service.findAll();
-            if(!found.isEmpty()){
-                return new ResponseEntity<>(found, null, HttpStatus.OK);
-            } else return new ResponseEntity<>(found, null, HttpStatus.NO_CONTENT);
-        }
-        catch (Exception e){
-            throw new Exception(e);
+    @DeleteMapping(path = "/{uuid}")
+    public final ResponseEntity<String> deleteRoom(@RequestParam UUID uuid){
+        if(this.service.delete(uuid)){
+            return new ResponseEntity<>("Successfully deleted",HttpStatus.OK);
+        }else{
+            return new ResponseEntity<>("Fail to delete",HttpStatus.BAD_REQUEST);
         }
     }
-
-    @GetMapping("/{id}")
-    public ResponseEntity<?> findById(@PathVariable Long id) throws Exception {
-        try{
-            User found = service.findById(id);
-            if(found != null) return new ResponseEntity<>(found, null, HttpStatus.OK);
-            else return new ResponseEntity<>(null, null,HttpStatus.NOT_FOUND);
-        }
-        catch (Exception e){
-            throw new Exception(e);
-        }
-    }
-
-    @PutMapping("/{id}")
-    public ResponseEntity<?> update(@PathVariable Long id, @RequestBody User room) throws Exception {
-        try{
-            if(service.findById(id) != null) {
-                room.setId(id);
-                return new ResponseEntity<>(service.update(room), null, HttpStatus.OK);
-            } else return new ResponseEntity<>(null, null, HttpStatus.NOT_FOUND);
-        }
-        catch (Exception e) {
-            throw new Exception(e);
-        }
-    }
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity<?> delete(@PathVariable Long id) throws Exception {
-        try{
-            if(service.findById(id) != null) {
-                service.delete(id);
-                return new ResponseEntity<>(null, null, HttpStatus.OK);
-            } else return new ResponseEntity<>(null, null, HttpStatus.NOT_FOUND);
-        }
-        catch (Exception e) {
-            throw new Exception(e);
-        }
-    }
-}
--- back-end/src/main/java/com/gestaocis/backend/services/RoomService.java
+++ back-end/src/main/java/com/gestaocis/backend/services/RoomService.java
@@ -1,5 +1,85 @@
 package com.gestaocis.backend.services;
 
+import com.gestaocis.backend.enums.Role;
+import com.gestaocis.backend.enums.RoleEntity;
+import com.gestaocis.backend.exceptions.BadRequestException;
+import com.gestaocis.backend.models.User;
+import com.gestaocis.backend.repositories.HealthInsuranceRepository;
+import com.gestaocis.backend.repositories.RoleEntityRepository;
+import com.gestaocis.backend.repositories.RoomRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
 public class RoomService {
 
-}
+    @Autowired
+    private RoomRepository userRepository;
+
+    @Autowired
+    private RoleEntityRepository roleEntityRepository;
+
+    @Autowired
+    private AddressService addressService;
+
+    @Autowired
+    private AddressRepository addressRepository;
+
+    @Autowired
+    private HealthInsuranceRepository healthInsuranceRepository;
+
+    // SAVES NEW ROOM
+    public RoomResponseDTO save(NewRoomRequestDTO room) throws Exception {
+
+        RoleEntity role = roleEntityRepository
+                .findByRoleName(Role.ROLE_ROOM)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Role not found"));
+
+     // LIST (supposedly kkk)
+        public List<RoomResponseDTO> findAll(){
+            try{
+                RoleEntity role = this.roleEntityRepository
+                        .findByRoleName(Role.ROLE_ROOM)
+                        .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Role Not Found"));
+                List<User> room = this.userRepository.findByRole(role);
+                return room.stream().map(RoomResponseDTO::new).collect(Collectors.toList());
+            }catch (Exception exception){
+                throw new BadRequestException(exception.getMessage());
+            }
+        }
+
+        // achados por UUID
+        public RoomResponseDTO findByUUID(UUID uuid){
+            try{
+                User room = this.userRepository.findByUuid(uuid)
+                        .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Room not Found, please check again"));
+                return new RoomResponseDTO(room);
+            }catch (Exception exception){
+                throw new BadRequestException(exception.getMessage());
+            }
+        }
+
+        //DELETE (finding by UUID)
+        public boolean delete(UUID uuid){
+            try{
+                User room = this.userRepository.findByUuid(uuid)
+                        .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Room not Found, please check again"));
+                this.userRepository.delete(room);
+
+                if(userRepository.findByUuid(uuid).isEmpty()) {
+                    return true;
+                }else{
+                    return false;
+                }
+
+            }catch (Exception exception){
+                throw new BadRequestException(exception.getMessage());
+            }
+        }
+
+    }

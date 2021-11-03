8b1606d1ab880b8dd569c6e8380773c7e5c77700
Fabio Frasson <fabio.frass@gmail.com>
2021-11-03 19:22:34 -0300

    Minor changes

--- back-end/src/main/java/com/gestaocis/backend/services/SecretaryService.java
+++ back-end/src/main/java/com/gestaocis/backend/services/SecretaryService.java
@@ -24,147 +24,179 @@ import java.util.stream.Collectors;
 @Service
 public class SecretaryService {
 
-    @Autowired
-    private UserRepository userRepository;
+  @Autowired private UserRepository userRepository;
 
-    @Autowired
-    private RoleEntityRepository roleEntityRepository;
+  @Autowired private RoleEntityRepository roleEntityRepository;
 
-    @Autowired
-    private AddressService addressService;
+  @Autowired private AddressService addressService;
 
-    @Autowired private AddressRepository addressRepository;
+  @Autowired private AddressRepository addressRepository;
 
-    // SAVE NEW SECRETARY
-    public SecretaryResponseDTO save(NewSecretaryRequestDTO secretary) throws Exception {
+  // SAVE NEW SECRETARY
+  public SecretaryResponseDTO save(NewSecretaryRequestDTO secretary) throws Exception {
 
-        RoleEntity role = roleEntityRepository
-                .findByRoleName(Role.ROLE_SECRETARY)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Role not found"));
+    RoleEntity role =
+        roleEntityRepository
+            .findByRoleName(Role.ROLE_SECRETARY)
+            .orElseThrow(
+                () -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Role not found"));
 
+    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
 
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+    try {
+      Address address =
+          addressService.save(
+              CepService.convertCepToAddress(CepService.formatCep(secretary.getCep())));
 
-        try {
-            Address address = addressService.save(CepService.convertCepToAddress(CepService.formatCep(secretary.getCep())));
+      User secretaryUser =
+          User.builder()
+              .uuid(UUID.randomUUID())
+              .cpf(secretary.getCpf())
+              .rg(secretary.getRg())
+              .email(secretary.getEmail())
+              .phone(secretary.getPhone())
+              .fullName(secretary.getFullName())
+              .birthdate(sdf.parse(secretary.getBirthdate()).toInstant())
+              .sex(secretary.getSex())
+              .addressLine2(secretary.getAddressLine2())
+              .addressCountry(secretary.getAddressCountry())
+              .password(new BCryptPasswordEncoder().encode(secretary.getPassword()))
+              .role(role)
+              .address(address)
+              .active(true)
+              .build();
 
-            User secretaryUser = User
-                    .builder()
-                    .uuid(UUID.randomUUID())
-                    .cpf(secretary.getCpf())
-                    .rg(secretary.getRg())
-                    .email(secretary.getEmail())
-                    .phone(secretary.getPhone())
-                    .fullName(secretary.getFullName())
-                    .birthdate(sdf.parse(secretary.getBirthdate()).toInstant())
-                    .sex(secretary.getSex())
-                    .addressLine2(secretary.getAddressLine2())
-                    .addressCountry(secretary.getAddressCountry())
-                    .password(new BCryptPasswordEncoder().encode(secretary.getPassword()))
-                    .role(role)
-                    .address(address)
-                    .active(true)
-                    .build();
-
-
-            return new SecretaryResponseDTO(userRepository.save(secretaryUser));
-        }catch (Exception exception){
-            throw new BadRequestException(exception.getMessage());
-        }
+      return new SecretaryResponseDTO(userRepository.save(secretaryUser));
+    } catch (Exception exception) {
+      throw new BadRequestException(exception.getMessage());
     }
+  }
 
-    // FIND BY UUID
-    public SecretaryResponseDTO findByUUID(UUID uuid){
-        try{
-            User secretary = this.userRepository.findByUuid(uuid)
-                    .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Secretary not Found, please check your uuid again"));
-            return new SecretaryResponseDTO(secretary);
-        }catch (Exception exception){
-            throw new BadRequestException(exception.getMessage());
-        }
+  // FIND BY UUID
+  public SecretaryResponseDTO findByUUID(UUID uuid) {
+    try {
+      User secretary =
+          this.userRepository
+              .findByUuid(uuid)
+              .orElseThrow(
+                  () ->
+                      new ResponseStatusException(
+                          HttpStatus.BAD_REQUEST,
+                          "Secretary not Found, please check your uuid again"));
+      return new SecretaryResponseDTO(secretary);
+    } catch (Exception exception) {
+      throw new BadRequestException(exception.getMessage());
     }
+  }
 
-    // FIND BY EMAIL
-    public SecretaryResponseDTO findByEmail(String email){
-        try{
-            User secretary = this.userRepository.findByEmail(email)
-                    .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Secretary not Found, please check your uuid again"));
-            return new SecretaryResponseDTO(secretary);
-        }catch (Exception exception){
-            throw new BadRequestException(exception.getMessage());
-        }
+  // FIND BY EMAIL
+  public SecretaryResponseDTO findByEmail(String email) {
+    try {
+      User secretary =
+          this.userRepository
+              .findByEmail(email)
+              .orElseThrow(
+                  () ->
+                      new ResponseStatusException(
+                          HttpStatus.BAD_REQUEST,
+                          "Secretary not Found, please check your uuid again"));
+      return new SecretaryResponseDTO(secretary);
+    } catch (Exception exception) {
+      throw new BadRequestException(exception.getMessage());
     }
+  }
 
-    // FIND ALL BY FULL NAME
-    public List<SecretaryResponseDTO> findByFullName(String name){
-        try{
-            List<User> secretariesFound = this.userRepository.findByFullNameContainingIgnoreCase(name);
-            return secretariesFound.stream().map(SecretaryResponseDTO::new).collect(Collectors.toList());
-        }catch (Exception exception){
-            throw new BadRequestException(exception.getMessage());
-        }
+  // FIND ALL BY FULL NAME
+  public List<SecretaryResponseDTO> findByFullName(String name) {
+    try {
+      List<User> secretariesFound = this.userRepository.findByFullNameContainingIgnoreCase(name);
+      return secretariesFound.stream().map(SecretaryResponseDTO::new).collect(Collectors.toList());
+    } catch (Exception exception) {
+      throw new BadRequestException(exception.getMessage());
     }
+  }
 
-    // FIND ALL BY ROLE
-    public List<SecretaryResponseDTO> findByRole(){
-        try{
-            RoleEntity role = this.roleEntityRepository
-                    .findByRoleName(Role.ROLE_SECRETARY)
-                    .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Role Not Found"));
-            List<User> secretaries = this.userRepository.findByRole(role);
-            return secretaries.stream().map(SecretaryResponseDTO::new).collect(Collectors.toList());
-        }catch (Exception exception){
-            throw new BadRequestException(exception.getMessage());
-        }
+  // FIND ALL BY ROLE
+  public List<SecretaryResponseDTO> findByRole() {
+    try {
+      RoleEntity role =
+          this.roleEntityRepository
+              .findByRoleName(Role.ROLE_SECRETARY)
+              .orElseThrow(
+                  () -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Role Not Found"));
+      List<User> secretaries = this.userRepository.findByRole(role);
+      return secretaries.stream().map(SecretaryResponseDTO::new).collect(Collectors.toList());
+    } catch (Exception exception) {
+      throw new BadRequestException(exception.getMessage());
     }
+  }
 
-    // FIND BY CPF
-    public SecretaryResponseDTO findByCpf(String cpf){
-        try{
-            User secretary = this.userRepository.findByCpf(cpf)
-                    .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Secretary not Found, please check your uuid again"));
-            return new SecretaryResponseDTO(secretary);
-        }catch (Exception exception){
-            throw new BadRequestException(exception.getMessage());
-        }
+  // FIND BY CPF
+  public SecretaryResponseDTO findByCpf(String cpf) {
+    try {
+      User secretary =
+          this.userRepository
+              .findByCpf(cpf)
+              .orElseThrow(
+                  () ->
+                      new ResponseStatusException(
+                          HttpStatus.BAD_REQUEST,
+                          "Secretary not Found, please check your uuid again"));
+      return new SecretaryResponseDTO(secretary);
+    } catch (Exception exception) {
+      throw new BadRequestException(exception.getMessage());
     }
+  }
 
-    // FIND BY UUID AND UPDATE SECRETARY
-    public SecretaryResponseDTO update (UUID uuid, NewSecretaryRequestDTO secretary){
-        try{
-            User secretaryFound = this.userRepository.findByUuid(uuid)
-                    .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Secretary not Found, please check your uuid again"));
+  // FIND BY UUID AND UPDATE SECRETARY
+  public SecretaryResponseDTO update(UUID uuid, NewSecretaryRequestDTO secretary) {
+    try {
+      User secretaryFound =
+          this.userRepository
+              .findByUuid(uuid)
+              .orElseThrow(
+                  () ->
+                      new ResponseStatusException(
+                          HttpStatus.BAD_REQUEST,
+                          "Secretary not Found, please check your uuid again"));
 
-            Address address = addressService.save(CepService.convertCepToAddress(CepService.formatCep(secretary.getCep())));
-            secretaryFound.setFullName(secretary.getFullName());
-            secretaryFound.setEmail(secretary.getEmail());
-            secretaryFound.setPhone(secretary.getPhone());
-            secretaryFound.setAddress(address);
-            secretaryFound.setAddressCountry(secretary.getAddressCountry());
-            secretaryFound.setAddressLine2(secretary.getAddressLine2());
+      Address address =
+          addressService.save(
+              CepService.convertCepToAddress(CepService.formatCep(secretary.getCep())));
+      secretaryFound.setFullName(secretary.getFullName());
+      secretaryFound.setEmail(secretary.getEmail());
+      secretaryFound.setPhone(secretary.getPhone());
+      secretaryFound.setAddress(address);
+      secretaryFound.setAddressCountry(secretary.getAddressCountry());
+      secretaryFound.setAddressLine2(secretary.getAddressLine2());
 
-            return new SecretaryResponseDTO(this.userRepository.save(secretaryFound));
-        }catch (Exception exception){
-            throw new BadRequestException(exception.getMessage());
-        }
+      return new SecretaryResponseDTO(this.userRepository.save(secretaryFound));
+    } catch (Exception exception) {
+      throw new BadRequestException(exception.getMessage());
     }
+  }
 
-    // FIND BY UUID AND DELETE SECRETARY
-    public boolean delete(UUID uuid){
-        try{
-            User secretary = this.userRepository.findByUuid(uuid)
-                    .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Secretary not Found, please check your uuid again"));
-            this.userRepository.delete(secretary);
+  // FIND BY UUID AND DELETE SECRETARY
+  public boolean delete(UUID uuid) {
+    try {
+      User secretary =
+          this.userRepository
+              .findByUuid(uuid)
+              .orElseThrow(
+                  () ->
+                      new ResponseStatusException(
+                          HttpStatus.BAD_REQUEST,
+                          "Secretary not Found, please check your uuid again"));
+      this.userRepository.delete(secretary);
 
-            if(userRepository.findByUuid(uuid).isEmpty()) {
-                return true;
-            }else{
-                return false;
-            }
+      if (userRepository.findByUuid(uuid).isEmpty()) {
+        return true;
+      } else {
+        return false;
+      }
 
-        }catch (Exception exception){
-            throw new BadRequestException(exception.getMessage());
-        }
+    } catch (Exception exception) {
+      throw new BadRequestException(exception.getMessage());
     }
-
+  }
 }
--- back-end/src/test/java/com/gestaocis/backend/integration/PatientIntegrationTest.java
+++ back-end/src/test/java/com/gestaocis/backend/integration/PatientIntegrationTest.java
@@ -2,13 +2,14 @@ package com.gestaocis.backend.integration;
 
 import com.gestaocis.backend.DTOs.PatientDTOs.NewPatientRequestDTO;
 import com.gestaocis.backend.DTOs.PatientDTOs.PatientResponseDTO;
+import com.gestaocis.backend.enums.Role;
+import com.gestaocis.backend.enums.RoleEntity;
 import com.gestaocis.backend.models.Address;
 import com.gestaocis.backend.models.User;
 import com.gestaocis.backend.repositories.AddressRepository;
 import com.gestaocis.backend.repositories.RoleEntityRepository;
 import com.gestaocis.backend.repositories.UserRepository;
 import com.gestaocis.backend.util.PatientCreator;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -24,28 +25,31 @@ import org.springframework.test.annotation.DirtiesContext;
 import java.util.Objects;
 import java.util.UUID;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
 public class PatientIntegrationTest {
 
-    @Autowired
-    private TestRestTemplate testRestTemplate;
+  @Autowired private TestRestTemplate testRestTemplate;
 
-    @Autowired
-    private RoleEntityRepository roleEntityRepository;
+  @Autowired private RoleEntityRepository roleEntityRepository;
 
-    @Autowired
-    private UserRepository userRepository;
+  @Autowired private UserRepository userRepository;
 
-    @Autowired
-    private AddressRepository addressRepository;
+  @Autowired private AddressRepository addressRepository;
 
-    private UUID uuid;
+  private UUID uuid;
 
-    @BeforeEach
-    void setUp() throws Exception {
-        Address address = addressRepository.save(Address.builder()
+  @BeforeEach
+  void setUp() throws Exception {
+    RoleEntity role =
+        roleEntityRepository.save(RoleEntity.builder().roleName(Role.ROLE_PATIENT).build());
+
+    Address address =
+        addressRepository.save(
+            Address.builder()
                 .cep("85853330")
                 .street("rua acacio pedroso")
                 .city("foz do iguaçu")
@@ -53,139 +57,151 @@ public class PatientIntegrationTest {
                 .neighborhood("bairro yolanda")
                 .build());
 
-        User userToBeSaved = PatientCreator.createPatientToBeSaved();
-        userToBeSaved.setAddress(address);
-        userRepository.save(userToBeSaved);
+    User userToBeSaved = PatientCreator.createPatientToBeSaved();
+    userToBeSaved.setAddress(address);
+    userToBeSaved.setRole(role);
+    userRepository.save(userToBeSaved);
 
-        uuid = userToBeSaved.getUuid();
-    }
+    uuid = userToBeSaved.getUuid();
+  }
 
-    @Test
-    @DisplayName("save Returns Saved Patient DTO when successful")
-    public void save_returnsSavedPatientDto_whenSuccessful(){
+  @Test
+  @DisplayName("save Returns Saved Patient DTO when successful")
+  public void save_returnsSavedPatientDto_whenSuccessful() {
 
-        NewPatientRequestDTO patientToBeCreatedDTO = NewPatientRequestDTO.builder()
-                .cpf("99999999991")
-                .rg("88888888881")
-                .email("integrationtest@test.com")
-                .phone("(45)999666999")
-                .fullName("Paciente Dodói da Silva")
-                .birthdate("1993-12-29")
-                .sex('F')
-                .addressCountry("Brazel")
-                .addressLine2("Rua da minha casa")
-                .password("senha123")
-                .cep("85858150")
-                .street("Rua teste")
-                .city("Foz do Iguaçu")
-                .uf("PR")
-                .neighborhood("Bairro da minha casa")
-                .build();
+    NewPatientRequestDTO patientToBeCreatedDTO =
+        NewPatientRequestDTO.builder()
+            .cpf("99999999991")
+            .rg("88888888881")
+            .email("integrationtest@test.com")
+            .phone("(45)999666999")
+            .fullName("Paciente Dodói da Silva")
+            .birthdate("1993-12-29")
+            .sex('F')
+            .addressCountry("Brazel")
+            .addressLine2("Rua da minha casa")
+            .password("senha123")
+            .cep("85858150")
+            .street("Rua teste")
+            .city("Foz do Iguaçu")
+            .uf("PR")
+            .neighborhood("Bairro da minha casa")
+            .build();
 
-        ResponseEntity<PatientResponseDTO> response = testRestTemplate
-                .postForEntity("/api/pacientes", patientToBeCreatedDTO, PatientResponseDTO.class);
+    ResponseEntity<PatientResponseDTO> response =
+        testRestTemplate.postForEntity(
+            "/api/pacientes", patientToBeCreatedDTO, PatientResponseDTO.class);
 
-        Assertions.assertThat(response).isNotNull();
+    assertThat(response).isNotNull();
 
-        Assertions.assertThat(response.getStatusCode().value()).isEqualTo(201);
-        Assertions.assertThat(Objects.requireNonNull(response.getBody()).getEmail()).isEqualTo(patientToBeCreatedDTO.getEmail());
-    }
+    assertThat(response.getStatusCode().value()).isEqualTo(201);
+    assertThat(Objects.requireNonNull(response.getBody()).getEmail())
+        .isEqualTo(patientToBeCreatedDTO.getEmail());
+  }
 
-    @Test
-    @DisplayName("findByUuid returns PatientDTO when successful")
-    public void findByUuid_returnsPatientDTO_whenSuccessful(){
-        ResponseEntity<PatientResponseDTO> response = testRestTemplate
-                .exchange("/pacientes/id/{uuid}",
-                HttpMethod.GET, null, PatientResponseDTO.class, uuid);
+  @Test
+  @DisplayName("findByUuid returns PatientDTO when successful")
+  public void findByUuid_returnsPatientDTO_whenSuccessful() {
+    ResponseEntity<PatientResponseDTO> response =
+        testRestTemplate.exchange(
+            "/api/pacientes/id/{uuid}", HttpMethod.GET, null, PatientResponseDTO.class, uuid);
 
-        Assertions.assertThat(response).isNotNull();
+    assertThat(response).isNotNull();
 
-        Assertions.assertThat(response.getStatusCode().value()).isEqualTo(200);
+    assertThat(response.getStatusCode().value()).isEqualTo(200);
 
-        Assertions.assertThat(Objects.requireNonNull(response.getBody()).getUuid()).isEqualTo(uuid);
-    }
+    assertThat(Objects.requireNonNull(response.getBody()).getUuid()).isEqualTo(uuid);
+  }
 
-    @Test
-    @DisplayName("findByEmail returns PatientDTO when successful")
-    public void findByEmail_returnsPatientDTO_whenSuccessful(){
-        String email = PatientCreator.createValidPatientResponseDTOSaved().getEmail();
+  @Test
+  @DisplayName("findByEmail returns PatientDTO when successful")
+  public void findByEmail_returnsPatientDTO_whenSuccessful() {
+    String email = PatientCreator.createValidPatientResponseDTOSaved().getEmail();
 
-        ResponseEntity<PatientResponseDTO> response = testRestTemplate
-                .getForEntity("/api/pacientes/email/" + email, PatientResponseDTO.class);
+    ResponseEntity<PatientResponseDTO> response =
+        testRestTemplate.getForEntity("/api/pacientes/email/" + email, PatientResponseDTO.class);
 
-        Assertions.assertThat(response).isNotNull();
+    assertThat(response).isNotNull();
 
-        Assertions.assertThat(response.getStatusCode().value()).isEqualTo(200);
+    assertThat(response.getStatusCode().value()).isEqualTo(200);
 
-        Assertions.assertThat(Objects.requireNonNull(response.getBody()).getEmail()).isEqualTo(email);
-    }
+    assertThat(Objects.requireNonNull(response.getBody()).getEmail()).isEqualTo(email);
+  }
 
-    @Test
-    @DisplayName("findByName returns list of PatientDTO when successful")
-    public void findByName_returnsListOfPatientDTO_whenSuccessful(){
-        String email = PatientCreator.createValidPatientResponseDTOSaved().getEmail();
+  @Test
+  @DisplayName("findByName returns list of PatientDTO when successful")
+  public void findByName_returnsListOfPatientDTO_whenSuccessful() {
+    String email = PatientCreator.createValidPatientResponseDTOSaved().getEmail();
 
-        String name = PatientCreator.createValidPatientResponseDTOSaved().getFullName();
+    String name = PatientCreator.createValidPatientResponseDTOSaved().getFullName();
 
-        ResponseEntity<PatientResponseDTO[]> response = testRestTemplate.getForEntity("/api/pacientes/name/" + name,PatientResponseDTO[].class);
+    ResponseEntity<PatientResponseDTO[]> response =
+        testRestTemplate.getForEntity("/api/pacientes/name/" + name, PatientResponseDTO[].class);
 
-        Assertions.assertThat(response).isNotNull();
+    assertThat(response).isNotNull();
 
-        Assertions.assertThat(response.getStatusCode().value()).isEqualTo(200);
+    assertThat(response.getStatusCode().value()).isEqualTo(200);
+  }
 
-    }
+  @Test
+  @DisplayName("findAll Returns List of Patients DTO when successful")
+  public void findAll_returnListOfPatientDTO_WhenSuccessful() {
+    ResponseEntity<PatientResponseDTO[]> response =
+        testRestTemplate.getForEntity("/api/pacientes", PatientResponseDTO[].class);
 
-    @Test
-    @DisplayName("findAll Returns List of Patients DTO when successful")
-    public void findAll_returnListOfPatientDTO_WhenSuccessful() {
-        ResponseEntity<PatientResponseDTO[]> response = testRestTemplate.getForEntity("/api/pacientes",PatientResponseDTO[].class);
+    assertThat(response).isNotNull();
 
-        Assertions.assertThat(response)
-                .isNotNull();
+    assertThat(response.getStatusCode().value()).isEqualTo(200);
+  }
 
-        Assertions.assertThat(response.getStatusCode().value()).isEqualTo(200);
-    }
+  @Test
+  @DisplayName("findByCpf returns Patient DTO when successful")
+  public void findByCpf_returnsPatientDTO_whenSuccessful() {
+    String cpf = PatientCreator.createPatientToBeSaved().getCpf();
 
-    @Test
-    @DisplayName("findByCpf returns Patient DTO when successful")
-    public void findByCpf_returnsPatientDTO_whenSuccessful(){
-        String cpf = PatientCreator.createPatientToBeSaved().getCpf();
+    ResponseEntity<PatientResponseDTO> response =
+        testRestTemplate.getForEntity("/api/pacientes/cpf/" + cpf, PatientResponseDTO.class);
 
-        ResponseEntity<PatientResponseDTO> response = testRestTemplate.getForEntity("/api/pacientes/cpf/" + cpf, PatientResponseDTO.class);
+    assertThat(response).isNotNull();
 
-        Assertions.assertThat(response).isNotNull();
+    assertThat(response.getStatusCode().value()).isEqualTo(200);
 
-        Assertions.assertThat(response.getStatusCode().value()).isEqualTo(200);
+    assertThat(Objects.requireNonNull(response.getBody()).getCpf()).isEqualTo(cpf);
+  }
 
-        Assertions.assertThat(Objects.requireNonNull(response.getBody()).getCpf()).isEqualTo(cpf);
-    }
+  @Test
+  @DisplayName("updatePatient return updated Patient DTO when successful")
+  public void updatePatient_returnUpdatedPatientDTO_whenSuccessful() {
 
-    @Test
-    @DisplayName("updatePatient return updated Patient DTO when successful")
-    public void updatePatient_returnUpdatedPatientDTO_whenSuccessful(){
+    NewPatientRequestDTO patientToBeUpdated =
+        NewPatientRequestDTO.builder()
+            .email("test2@test.com")
+            .phone("(45)9995595959")
+            .fullName("Dodói Teste da Silva")
+            .cep("85858150")
+            .mothersName("Mamai Do Dodói")
+            .addressLine2("blablabla")
+            .addressCountry("teste")
+            .build();
+    ResponseEntity<NewPatientRequestDTO> response =
+        testRestTemplate.exchange(
+            "/api/pacientes/{uuid}",
+            HttpMethod.PUT,
+            new HttpEntity<>(patientToBeUpdated),
+            NewPatientRequestDTO.class,
+            uuid);
 
-        NewPatientRequestDTO patientToBeUpdated = NewPatientRequestDTO.builder()
-                .email("test2@test.com")
-                .phone("(45)9995595959")
-                .fullName("Dodói Teste da Silva")
-                .cep("85858150")
-                .mothersName("Mamai Do Dodói")
-                .addressLine2("blablabla")
-                .addressCountry("teste")
-                .build();
-        ResponseEntity<NewPatientRequestDTO> response = testRestTemplate.exchange("/api/pacientes/{uuid}", HttpMethod.PUT, new HttpEntity<>(patientToBeUpdated), NewPatientRequestDTO.class, uuid);
+    assertThat(response).isNotNull();
+  }
 
+  @Test
+  @DisplayName("deletePatient returns String when successful")
+  public void deletePatient_returnUpdatedPatientDTO_whenSuccessful() {
 
-        Assertions.assertThat(response).isNotNull();
-    }
+    ResponseEntity<String> response =
+        testRestTemplate.exchange(
+            "/api/pacientes/{uuid}", HttpMethod.DELETE, null, String.class, uuid);
 
-    @Test
-    @DisplayName("deletePatient returns String when successful")
-    public void deletePatient_returnUpdatedPatientDTO_whenSuccessful(){
-
-        ResponseEntity<String> response = testRestTemplate.exchange("/api/pacientes/{uuid}", HttpMethod.DELETE, null, String.class, uuid);
-
-        Assertions.assertThat(response).isNotNull();
-
-    }
+    assertThat(response).isNotNull();
+  }
 }

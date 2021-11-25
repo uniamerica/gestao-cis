80789a4569dff63f313a82262c9b31e3271b9cc6
Marcelo Fortes <63818742+MarceloFortesGC@users.noreply.github.com>
2021-11-24 21:37:48 -0300

    Commit do Conserto
    Me desculpe senhores

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,13 +1,10 @@
 import './App.scss';
-// import Header from './Components/Header/Header';
-// import Sidebar from './Components/Sidebar/Sidebar';
 import Login from './Views/Login/Index';
 
 function App() {
   return (
     <>
     <div className='appContent'>
-      {/* <Sidebar /> */}
       <Login />
     </div>
     </>
--- front-end/src/Components/Sidebar/Sidebar.jsx
+++ front-end/src/Components/Sidebar/Sidebar.jsx
@@ -1,35 +1,9 @@
-import CalendarTodayOutlinedIcon from '@material-ui/icons/CalendarTodayOutlined'
-import PeopleAltOutlinedIcon from '@material-ui/icons/PeopleAltOutlined'
-import doctor from '../../assets/images/doctor.png'
-import PhoneOutlinedIcon from '@material-ui/icons/PhoneOutlined'
-import MonetizationOnOutlinedIcon from '@material-ui/icons/MonetizationOnOutlined'
-import { SidebarStyle } from './style'
+import { Fragment } from "react";
 
 export default function Sidebar() {
     return(
-        <SidebarStyle>
-            <div className='content' >
-                <div className='combo'>
-                    <CalendarTodayOutlinedIcon className='icon' />
-                    <p className='title'>Agendamentos</p>
-                </div>
-                <div className='combo'>
-                    <PeopleAltOutlinedIcon className='icon' />
-                    <p className='title'>Pacientes</p>
-                </div>
-                <div className='combo'>
-                    <img className='doctor' src={doctor} />
-                    <p className='title'>Profissionais</p>
-                </div>
-                <div className='combo'>
-                    <PhoneOutlinedIcon className='icon' />
-                    <p className='title'>Secretárias</p>
-                </div>
-                <div className='combo'>
-                    <MonetizationOnOutlinedIcon className='icon' />
-                    <p className='title'>Pagamentos</p>
-                </div>
-            </div>
-        </SidebarStyle>
+       <Fragment>
+           
+       </Fragment>
     )
 }

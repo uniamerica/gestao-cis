5347de47d961912cf0a6c2a9189f4c9050f0355f
Fabio Frasson <fabio.frass@gmail.com>
2021-11-27 10:35:42 -0300

    Room routes

--- cis-firebase/src/routes/roomRoutes.js
+++ cis-firebase/src/routes/roomRoutes.js
@@ -5,6 +5,27 @@ const authMiddleware = require("../middlewares/authMiddleware");
 const roomRoutes = Router();
 
 roomRoutes.get("/", authMiddleware.healthProfessional, roomController.index);
+roomRoutes.get(
+  "/find",
+  authMiddleware.healthProfessional,
+  roomController.findByName
+);
+roomRoutes.get(
+  "/:id",
+  authMiddleware.healthProfessional,
+  roomController.findById
+);
+roomRoutes.post("/", authMiddleware.healthProfessional, roomController.create);
+roomRoutes.put(
+  "/:id",
+  authMiddleware.healthProfessional,
+  roomController.update
+);
+roomRoutes.delete(
+  "/:id",
+  authMiddleware.healthProfessional,
+  roomController.delete
+);
 
 module.exports = {
   roomRoutes,

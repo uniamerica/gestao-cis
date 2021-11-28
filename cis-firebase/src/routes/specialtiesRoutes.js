caf7f20ce9566ce3d5adf1baabd25971896cf6aa
Fabio Frasson <fabio.frass@gmail.com>
2021-11-28 12:58:19 -0300

    Specialties routes & index

--- cis-firebase/src/routes/specialtiesRoutes.js
+++ cis-firebase/src/routes/specialtiesRoutes.js
@@ -4,7 +4,36 @@ const authMiddleware = require("../middlewares/authMiddleware");
 
 const specialtiesRoutes = Router();
 
-specialtiesRoutes.get("/", specialtiesController.index);
+specialtiesRoutes.get(
+  "/",
+  authMiddleware.adminOrHealthProfessional,
+  specialtiesController.index
+);
+specialtiesRoutes.get(
+  "/find",
+  authMiddleware.adminOrHealthProfessional,
+  specialtiesController.findByTitle
+);
+specialtiesRoutes.get(
+  "/:id",
+  authMiddleware.adminOrHealthProfessional,
+  specialtiesController.findById
+);
+specialtiesRoutes.post(
+  "/",
+  authMiddleware.adminOrHealthProfessional,
+  specialtiesController.create
+);
+specialtiesRoutes.put(
+  "/",
+  authMiddleware.adminOrHealthProfessional,
+  specialtiesController.update
+);
+specialtiesRoutes.delete(
+  "/",
+  authMiddleware.adminOrHealthProfessional,
+  specialtiesController.delete
+);
 
 module.exports = {
   specialtiesRoutes,

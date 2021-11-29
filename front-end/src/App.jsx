84f64d2795fd7186ad03e7f1af1d29f2f00f23f7
alinezsigmond <alinezsigmond@hotmail.com>
2021-11-28 21:09:00 -0300

    Cadastro estático profissional de saúde

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -5,6 +5,7 @@ import Header from "./Components/Header/Header";
 import Sidebar from "./Components/Sidebar/Sidebar";
 import Home from "./Views/Home";
 import { NameForm } from './Views/Testes'
+import Professional from "./Views/Professional";
 
 function App() {
   const logged = {value: false}
@@ -22,6 +23,7 @@ function App() {
           <Route path="/" element={<Login />}/>
           <Route path="/home" element={<Home />} /> 
           <Route path="/testes" element={<NameForm />} /> 
+          <Route path="/profissionais" element={<Professional />} /> 
         </Routes>
       </BrowserRouter>
     </Fragment>

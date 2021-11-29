d3299edeffb2481e5d8c2897d1b296738e6df1ac
Fabio Frasson <fabio.frass@gmail.com>
2021-11-29 09:50:27 -0300

    Appointment service [missing findBy and create methods]

--- cis-firebase/src/services/appointmentService.js
+++ cis-firebase/src/services/appointmentService.js
@@ -29,4 +29,86 @@ module.exports = {
       throw new Error(error.message);
     }
   },
+
+  // FIND BY ID
+  findById: async function (id) {
+    try {
+      const snapshot = appointmentCollection.doc(id).get();
+      const appointment = snapshot.data();
+      if (!appointment) {
+        return {
+          error: "Appointment not found",
+        };
+      }
+      return appointment;
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  },
+
+  // FIND BY ?
+  // IMPLEMENTATION
+
+  // CREATE - VALIDATION BY?
+  // create: async function(obj) {
+  //  try {
+  //   const validData = appointmentSchema.validate(obj)
+
+  //   if(validData.error) {
+  //    return {
+  //     error: validData.error
+  //    }
+  //   }
+
+  //   const appointmentExistById = await this.findById(obj)
+  //  } catch (error) {
+
+  //  }
+  // }
+
+  // UPDATE
+  update: async function (id, appointment) {
+    try {
+      const found = await this.findById(id);
+      if (!!found.error) return found;
+
+      const updated = {
+        id: id,
+        date: !!appointment.date ? appointment.date : found.date,
+        time: !!appointment.time ? appointment.time : found.time,
+        room: !!appointment.room ? appointment.room : found.room,
+        observation: !!appointment.observation
+          ? appointment.observation
+          : observation.room,
+        patient: !!appointment.patient
+          ? appointment.patient
+          : observation.patient,
+        healthProfessional: !!appointment.healthProfessional
+          ? appointment.healthProfessional
+          : observation.healthProfessional,
+        price: !!appointment.price ? appointment.price : observation.price,
+        paid: !!appointment.paid ? appointment.paid : observation.paid,
+      };
+
+      const result = await appointmentCollection.doc(id).set(updated);
+
+      return result;
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  },
+
+  // DELETE
+  delete: async function (id) {
+    try {
+      const found = await this.findById(id);
+      if (!!found.error) return found;
+
+      const result = await appointmentCollection.doc(id).delete();
+
+      return result;
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  },
 };

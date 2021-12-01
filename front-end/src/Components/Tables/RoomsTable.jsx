e97298b0862b695b8e36452ee11eed77c204b178
Mariana10-04 <71359480+Mariana10-04@users.noreply.github.com>
2021-11-30 23:06:09 -0300

    Cadastro salas

--- front-end/src/Components/Tables/RoomsTable.jsx
+++ front-end/src/Components/Tables/RoomsTable.jsx
@@ -1,12 +1,62 @@
-import React from 'react'
-import { Container, Typography } from "@mui/material";
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
-export default function RoomsTable() {
-  return(
-    <React.Fragment>
-      <Container maxWidth="lg">
-        <Typography variant="h6" color="initial">RoomsTable funciona</Typography>
-      </Container>
-    </React.Fragment>
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: "#1CA78C",
+    color: theme.palette.common.white,
+    fontSize: 18,
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+    
+  },
+}));
+
+function createData(clinic, identification, category, occupation, role) {
+  return { clinic, identification, category, occupation, role };
+}
+
+const rows = [
+  createData('CIS-Foz do Iguaçu', "Sala 1", "Fisioterapia", "05", true),
+  createData('CIS-Foz do Iguaçu', "Sala 2", "Psicicologia", "02", false),  
+
+];
+
+export default function CustomizedTables() {
+  return (
+    <TableContainer component={Paper} sx={{ margin:"2rem 0"}}>
+      <Table sx={{ minWidth: 700 }} aria-label="customized table">
+        <TableHead>
+          <TableRow>
+            <StyledTableCell>Clinic</StyledTableCell>
+            <StyledTableCell align="center">Identificação</StyledTableCell>
+            <StyledTableCell align="center">Categoria</StyledTableCell>
+            <StyledTableCell align="center">Ocupação Max.</StyledTableCell>
+            <StyledTableCell align="center">Disponibilidade</StyledTableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow key={row.id}>
+              <StyledTableCell component="th" scope="row">
+                {row.clinic}
+              </StyledTableCell>
+              <StyledTableCell align="center">{row.identification}</StyledTableCell>
+              <StyledTableCell align="center">{row.category}</StyledTableCell>
+              <StyledTableCell align="center">{row.occupation}</StyledTableCell>
+              <StyledTableCell align="center">{row.role ? "Disponivel" : "Indisponivel"}</StyledTableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 }
--- front-end/src/Views/Rooms/index.jsx
+++ front-end/src/Views/Rooms/index.jsx
@@ -1,14 +1,136 @@
-import { Container, Typography } from "@mui/material";
-import React from 'react'
+import * as React from "react";
+import {
+  Box,
+  Button,
+  Modal,
+  Typography,
+  TextField,
+  Autocomplete,
+  Container,
+} from "@mui/material";
 import RoomsTable from "../../Components/Tables/RoomsTable";
 
+const modalStyle = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  minHeight: 600,
+  bgcolor: "background.paper",
+  boxShadow: 24,
+  p: 4,
+};
+
+const buttonStyle = {
+  marginTop: "2%",
+  borderRadius: 20,
+  backgroundColor: "#00939F",
+};
+
 export default function Rooms() {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
   return(
     <React.Fragment>
       <Container maxWidth="lg">
-        <Typography variant="h5" color="initial">Rooms funciona</Typography>
+      <Button
+          variant="contained"
+          style={buttonStyle}
+          onClick={handleOpen}
+          disableRipple
+        >
+          Cadastrar Sala
+        </Button>
         <RoomsTable />
       </Container>
+      <Modal
+        disableBackdropClick
+        open={open} // onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={modalStyle}>
+          <Box
+            component="form"
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+            }}
+          >
+            <Typography variant="h5" color="initial">
+              Nova Sala <br />
+            </Typography>
+            <TextField
+              required
+              type="text"
+              id="outlined-required"
+              label="Clinica"
+              sx={{ marginTop: "1.5rem" }}
+            />
+            <TextField
+              required
+              type="room"
+              id="outlined-required"
+              label="Identificação"
+              sx={{ marginTop: "1.5rem" }}
+            />
+            <Autocomplete
+              required
+              sx={{ marginTop: "1.5rem" }}
+              multiple
+              id="tags-outlined"
+              options={categoria}
+              getOptionLabel={(option) => option.name}
+              filterSelectedOptions
+              renderInput={(params) => (
+                <TextField
+                  {...params}
+                  label="Categoria"
+                  placeholder="Catogoria"
+                />
+              )}
+            />
+            <TextField
+              required
+              type="ocupMax"
+              id="outlined-required"
+              label="Ocupação Max."
+              sx={{ marginTop: "1.5rem" }}
+            />
+
+            <Button
+              type="submit"
+              variant="contained"
+              sx={{ marginTop: "2rem", backgroundColor: "#00939F" }}
+            >
+              Cadastrar
+            </Button>
+            <Button
+              type="reset"
+              variant="contained"
+              onClick={handleClose}
+              sx={{ marginTop: "2rem", backgroundColor: "#c3c3c3" }}
+            >
+              Cancelar
+            </Button>
+          </Box>
+        </Box>
+      </Modal>
     </React.Fragment>
   );
 }
+
+const categoria = [
+  { id: 0, name: "Acupuntura" },
+  { id: 1, name: "Psicologia" },
+  { id: 2, name: "Nutrição" },
+  { id: 3, name: "Educação Física" },
+  { id: 4, name: "Fonoaudiologia" },
+  { id: 5, name: "Farmácia" },
+  { id: 6, name: "Medicina" },
+  { id: 7, name: "Odontologia" },
+];

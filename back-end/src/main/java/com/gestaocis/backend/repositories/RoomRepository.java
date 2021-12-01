81d0ef4fb91a593173d58b15766e80a1f0092a2a
alinezsigmond <alinezsigmond@hotmail.com>
2021-12-01 11:11:23 -0300

    slightly improved crud

--- back-end/src/main/java/com/gestaocis/backend/repositories/RoomRepository.java
+++ back-end/src/main/java/com/gestaocis/backend/repositories/RoomRepository.java
@@ -5,8 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 public interface RoomRepository extends JpaRepository<Room, Long> {
 
     Optional<Room> findByRoomNumber(Integer number);
+    Optional<Room> findByUuid(UUID uuid);
 }
